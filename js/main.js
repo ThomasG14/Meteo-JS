aedc4dc48d71157728d37c54bc8088ea671a233b
Thomas Gourlaouen <68146526+ThomasG14@users.noreply.github.com>
2022-12-05 16:10:26 +0100

    Add indice value
    Add indice of humidity and wind direction

--- js/main.js
+++ js/main.js
@@ -17,116 +17,111 @@ const temps = document.querySelector('.temp');
 const temperature = document.querySelector('.temperature');
 //Humiditer
 const humidity = document.querySelector('.humidity');
+const humidity_index = document.querySelector('.humidity_index')
 //Indice UV
 const iuv = document.querySelector('.uv');
 //vent
 const wind = document.querySelector('.wind')
 const wind_Direction = document.querySelector('.wind-direction')
 
-if(navigator.geolocation)
-{
-    navigator.geolocation.getCurrentPosition(position => 
-    {
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
         let long = position.coords.longitude;
         let lat = position.coords.latitude;
         CallAPIWeather(long, lat);
         CallAPICity(long, lat);
-    },() => 
-    {
+    }, () => {
         alert(`Activez la localisation pour faire fonctionner l'appli`)
     })
 }
 
 //Appel de L'api pour la météo
-function CallAPIWeather(long, lat)
-{
+function CallAPIWeather(long, lat) {
     fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely&units=metric&lang=fr&appid=${APIKEY}`)
-    .then((reponse) =>
-    {
-        return reponse.json();
-    })
-    .then((data) => 
-    {
-        console.log(data);
+        .then((reponse) => {
+            return reponse.json();
+        })
+        .then((data) => {
+            console.log(data);
 
-        API_WEATHER = data;
+            API_WEATHER = data;
 
-        let heureNow = new Date().getHours()
-        let MinuteNow = new Date().getMinutes();
-        let uv = API_WEATHER.current.uvi;
-        let windDirection = API_WEATHER.current.wind_deg;
-
-
-        //Icone du temp dynamique
-        if(heureNow >= 6 && heureNow < 21)
-        {
-            imgIcon.src = `img/jour/${API_WEATHER.current.weather[0].icon}.svg`
-            background.style.backgroundColor='#5094ab';
-            hills.src = `img/bg-day.png`
-        }
-        else
-        {
-            imgIcon.src = `img/nuit/${API_WEATHER.current.weather[0].icon}.svg`
-            background.style.backgroundColor='#0f0f24';
-            hills.src = 'img/bg-night.png'
-        }
-
-        //Le temp
-        temps.innerHTML = API_WEATHER.current.weather[0].description;
-        //La température
-        temperature.innerHTML = `${Math.trunc(API_WEATHER.current.temp)}°`;
-        //L'humiditer
-        humidity.innerHTML = `${Math.trunc(API_WEATHER.current.humidity)} %`;
-        //Vent
-        wind.innerHTML = `${Math.trunc(API_WEATHER.current.wind_speed* 3.6) } Km/h`;
-
-        //echelle d'indice uv
-        if(uv <= 2)
-        {
-            iuv.innerHTML = `faible`;
-        }
-        else if(uv > 2 && uv <= 5)
-        {
-            iuv.innerHTML = `moyen`;
-        }
-        else if(uv > 5 && uv <= 7)
-        {
-            iuv.innerHTML = `élevé`;
-        }
-        else if(uv > 7 && uv <= 10)
-        {
-            iuv.innerHTML = `Très élevé`;
-        }
-        else
-        {
-            iuv.innerHTML = `extrème`;
-        }
+            let heureNow = new Date().getHours()
+            let MinuteNow = new Date().getMinutes();
+            let uv = API_WEATHER.current.uvi;
+            let value_humidity = `${Math.trunc(API_WEATHER.current.humidity)}`
+            let windDirection = API_WEATHER.current.wind_deg;
 
 
-        //direction du vent
-        if(windDirection === 0)
-        {
-            wind_Direction.innerHTML = `Nord`;
-        }
+            //Icone du temp dynamique
+            if (heureNow >= 6 && heureNow < 21) {
+                imgIcon.src = `img/jour/${API_WEATHER.current.weather[0].icon}.svg`
+                background.style.backgroundColor = '#5094ab';
+                hills.src = `img/bg-day.png`
+            }
+            else {
+                imgIcon.src = `img/nuit/${API_WEATHER.current.weather[0].icon}.svg`
+                background.style.backgroundColor = '#0f0f24';
+                hills.src = 'img/bg-night.png'
+            }
 
+            //Le temp
+            temps.innerHTML = API_WEATHER.current.weather[0].description;
+            //La température
+            temperature.innerHTML = `${Math.trunc(API_WEATHER.current.temp)}°`;
+            //L'humiditer
+            humidity.innerHTML = value_humidity + " %";
+            if (value_humidity >= 0 && value_humidity < 35) {
+                humidity_index.innerHTML = `Sec`;
+            }
+            else if (value_humidity >= 35 && value_humidity < 75) {
+                humidity_index.innerHTML = `Optimal`;
+            }
+            else if (value_humidity >= 75 && value_humidity <= 100) {
+                humidity_index.innerHTML = `Humide`;
+            }
+            //Vent
+            wind.innerHTML = `${Math.trunc(API_WEATHER.current.wind_speed * 3.6)} Km/h`;
 
+            //echelle d'indice uv
+            if (uv <= 2) {
+                iuv.innerHTML = `faible`;
+            }
+            else if (uv > 2 && uv <= 5) {
+                iuv.innerHTML = `moyen`;
+            }
+            else if (uv > 5 && uv <= 7) {
+                iuv.innerHTML = `élevé`;
+            }
+            else if (uv > 7 && uv <= 10) {
+                iuv.innerHTML = `Très élevé`;
+            }
+            else {
+                iuv.innerHTML = `extrème`;
+            }
 
-    })
+            //direction du vent
+            let Tab_Win_Direction = ["Nord", "Nord Nord Ouest", "Nord Est", "Est Nord Est",
+                "Est", "Est Sud Est", "Sud Est", "Sud Sud Est",
+                "Sud", "Sud Sud Ouest", "Sud ouest", "Ouest Sud Ouest",
+                "Ouest", "Ouest Nord Ouest", "Nord Ouest", "Nord Nord Ouest"]
+            windDirection = windDirection / 22.5
+            wind_Direction.innerHTML = Tab_Win_Direction[windDirection]
+            console.log(wind_Direction)
+
+        })
 }
 
 //Appel de l'api pour la localisation à partir de la longitude et de la lattitude
-function CallAPICity(longi, lati)
-{
+function CallAPICity(longi, lati) {
     fetch(`https://geo.api.gouv.fr/communes?lat=${lati}&lon=${longi}&fields=nom`)
-    .then((response) =>
-    {
-        return response.json();
-    })
-    .then((results) => 
-    {        
-        //console.log(results)
-        
-        API_CITY = results
-        location.innerText = API_CITY[0].nom;      
-    })
+        .then((response) => {
+            return response.json();
+        })
+        .then((results) => {
+            //console.log(results)
+
+            API_CITY = results
+            location.innerText = API_CITY[0].nom;
+        })
 }
